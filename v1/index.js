0de8cc58d58472d29ff9170dd2733ff29fe7bc98
Jeremiah-Quaynor <jeremiah.quaynor@stu.ucc.edu.gh>
2022-12-11 04:36:34 +0000

    adding frontend

--- v1/index.js
+++ v1/index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -9,8 +10,11 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended:false}))
 
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
 app.use('/openai', require("./routes/openaiRoutes"))
-
-
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
