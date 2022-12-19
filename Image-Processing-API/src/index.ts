81fb1d9cfc2fc5dafad866959f6b2bb60e8f317f
Jeremiah-Quaynor <jeremiah.quaynor@stu.ucc.edu.gh>
2022-12-19 00:25:25 +0000

    refactoring code

--- Image-Processing-API/src/index.ts
+++ Image-Processing-API/src/index.ts
@@ -1,23 +1,43 @@
 const path = require('path');
 const express = require('express');
-const port = 3000;
-import ApiImage from "./utilities/imageUtil";
+const fs = require('fs');
+const sharp = require('sharp')
 
+const port = 3000;
 const app = express();
 
 
 // enabling body parser
 app.use(express.json());
-app.use(express.urlencoded({ extended:false}))
-app.use(express.static('public'));
+// app.use(express.urlencoded({ extended:false}))
+// app.use(express.static('public'));
+
+let data: string[] ; 
+
+async function resizeImage (filename:string, width:number, height:number) {
+        await sharp(`src/assets/full/${filename}.jpg`)
+        .resize(width,height)
+        .toFile(`src/assets/thumb/${filename}-resized.jpg`);
+}
 
 
 
-app.use('/api',(_req: any,res: { send: (arg0: string) => void; })=> {
-    res.send("home route")
+
+app.get('/api/images', (req: any,res: { send: (arg0: string) => void; sendFile: (arg0: string) => void; })=>{
+    console.log(req.query)
+    if (Object.keys(req.query).length === 0){
+        res.send("Please enter a filename and a size")
+        return 
+    }else {
+        let filename = req.query.filename
+        let width = req.query.width
+        let height = req.query.height
+        resizeImage(filename, parseInt(width), parseInt(height))
+        // res.cache(3600)
+        res.sendFile(__dirname + `/assets/thumb/${filename}-resized.jpg`)
+    }
 })
 
-app.use('/image', ApiImage)
 
 
 app.listen(port, ()=> {
--- Image-Processing-API/src/utilities/imageUtil.ts
+++ Image-Processing-API/src/utilities/imageUtil.ts
@@ -1,33 +1,37 @@
-const  sourceDir = './src/assets/full'
-const fs = require('fs');
-const sharp = require('sharp')
-let data: string;
+// const  sourceDir = './src/assets/full'
+// const fs = require('fs');
+// const sharp = require('sharp')
+// let data: string;
 
 
 
-async function call () {
-    // await fs.readdir(sourceDir, (err: any,files: any) => {
-    //     if (err) {
-    //         console.log("error")
-    //     }
-    //     data = files[0]
+// async function call () {
+//     // await fs.readdir(sourceDir, (err: any,files: any) => {
+//     //     if (err) {
+//     //         console.log("error")
+//     //     }
+//     //     data = files[0]
     
-    // })
+//     // })
 
-    await sharp(`src/assets/full/saitama.jpg`)
-    .resize(300,200)
-    .toFile('src/assets/thumb/saitama.jpg');
-}
-
-
-function ApiImage (req: any, res: { sendFile: (arg0: string) => void; }){
-    call()
-    res.sendFile('/home/jeremiah/Projects/noodejs/Image-Processer/Image-Processing-API/src/assets/thumb/saitama.jpg')
-}
+//     await sharp(`src/assets/full/saitama.jpg`)
+//     .resize(300,200)
+//     .toFile('src/assets/thumb/saitama.jpg');
+// }
 
 
+// function ApiImage (req: any, res: { cache: (arg0: number) => void; sendFile: (arg0: string) => void; }){
+//     call()
+//     setTimeout(() => {
+        
+//     }, 2000);
+//     res.sendFile('/home/jeremiah/Projects/noodejs/Image-Processer/Image-Processing-API/src/assets/thumb/saitama.jpg')
+//     res.cache(3600)
+// }
 
 
 
 
-export default ApiImage
+
+
+// export default ApiImage
