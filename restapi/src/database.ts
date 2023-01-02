a610070623d9dff0ef7b11e2cda6feef8e398e29
Jeremiah-Quaynor <jeremiah.quaynor@stu.ucc.edu.gh>
2023-01-02 06:09:36 +0000

    bug fix

--- restapi/src/database.ts
+++ restapi/src/database.ts
@@ -1,15 +1,14 @@
 import { Pool } from 'pg'
 import  dotenv from 'dotenv'
-import { env } from 'process'
 
 dotenv.config()
 
 const connection = new Pool({
-    user: env.POSTGRES_USERNAME , 
-    host:  env.POSTGRES_HOST,
-    database:  env.POSTGRES_DATABASE,
-    password:  env.POSTGRES_PASSWORD,
-    port:  Number(env.POSTGRES_PORT)
+    user: process.env.POSTGRES_USERNAME , 
+    host:  process.env.POSTGRES_HOST,
+    database:  process.env.POSTGRES_DATABASE,
+    password:  process.env.POSTGRES_PASSWORD,
+    port:  Number(process.env.POSTGRES_PORT)
     
 })
 
